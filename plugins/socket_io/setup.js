7d36d3538c3ad99f2d3abe84dddcc28f0cdf8d8c
snoryder8019 <snoryder8019@gmail.com>
2024-03-05 17:14:02 -0700

    workz

--- plugins/socket_io/setup.js
+++ plugins/socket_io/setup.js
@@ -1,79 +1,67 @@
 
 const socketIo = require('socket.io');
-const sharedsession = require("express-socket.io-session");
-const passport = require('passport')
-const { sessionMiddleware } = require('../../app'); // Adjust the path as necessary
-const {sendConnectionMeta} = require('../mongo/socketFunctions/update')
+const sessionMiddleware = require('../../app').sessionMiddleware; // Assuming this exports your session middleware
+const passport = require('passport');
+const updateUsersList = require('../mongo/socketFunctions/update').sendConnectionMeta;
+
 module.exports.setupSocketIO = (server) => {
-    const io = socketIo(server);    
-    const mainChat = io.of('/main_chat'); 
+    const io = socketIo(server);
+    const mainChat = io.of('/main_chat');
     const users = {}; // Tracks connected users
-    mainChat.use((socket, next) => {
-    function onlyForHandshake(middleware) {
-        return (req, res, next) => {
-          const isHandshake = req._query.sid === undefined;
-          if (isHandshake) {
-            middleware(req, res, next);
-          } else {
-            next();
-          }
+
+    // Middleware for session handling
+    const wrapMiddlewareForSocketIO = (middleware) => {
+        return (socket, next) => {
+            middleware(socket.request, {}, next);
         };
-      }
-      sessionMiddleware(socket.request, {}, next);
-  
-      io.engine.use(onlyForHandshake(sessionMiddleware));
-      io.engine.use(onlyForHandshake(passport.session()));
-      io.engine.use(
-        onlyForHandshake((req, res, next) => {
-          if (req.user) {
+    };
+
+    // Apply middleware for session and passport
+    mainChat.use(wrapMiddlewareForSocketIO(sessionMiddleware));
+    mainChat.use(wrapMiddlewareForSocketIO(passport.session()));
+
+    // Reject connections without an authenticated user
+    mainChat.use((socket, next) => {
+        if (socket.request.user) {
             next();
-          } else {
-            res.writeHead(401);
-            res.end();
-          }
-        }),
-      );
+        } else {
+            next(new Error('Unauthorized'));
+        }
     });
-      mainChat.on('connection', (socket) => {
-        console.log('A user connected to main_chat');
-        const userName = socket.request.user ? socket.request.user.firstName : 'Anonymous';
-        socket.join('General');
-        updateUsersList(mainChat, 'General');
 
-        const socketInfo=socket
-        console.log(`userName: ${userName}, socketinfo:${socketInfo}`);
+    mainChat.on('connection', (socket) => {
+      const userName = socket.request.user.firstName;
+      console.log(`${userName} connected to main_chat`);
         users[socket.id] = userName; // Add user to the list
-//sendConnectionMeta('connection',user)
-        mainChat.emit('user list', Object.values(users));
-        // Listen for video streaming data from clients
-        socket.on('stream', (data) => {
-            // Broadcast the video data to all connected clients
-            io.emit('stream', data);
+        socket.join('General');
+
+        // Notify all clients in the namespace about the updated user list
+        mainChat.to('General').emit('user list', Object.values(users));
+
+        // Handle chat messages
+        socket.on('chat message', (msg) => {
+            // Broadcast the message to all clients in "General" room within the main_chat namespace
+            mainChat.to('General').emit('chat message', { text: msg, user: socket.request.user });
         });
 
-        // Listen for chat messages from clients
-        socket.on('chat message', (msg) => {
-          mainChat.to('General').emit('chat message', userName + ': ' + msg);
-          console.log(msg)
-      });
-       socket.on('join room', (room) => {
+        // Handle joining specific rooms
+        socket.on('join room', (room) => {
             socket.join(room);
-            // Update users list for the room
-            updateUsersList(mainChat, room);
+            // Optionally, update users list for the room
         });
+
+        // Handle chat messages in specific rooms
         socket.on('chat message in room', ({ room, msg }) => {
-          console.log(`message in ${room}: ${msg}`);
-          mainChat.to(room).emit('chat message', msg);
+            mainChat.to(room).emit('chat message', { text: msg, user: socket.request.user });
         });
+
         // Handle disconnection
         socket.on('disconnect', () => {
-          // Update users list upon disconnect
-          updateUsersList(mainChat, 'General'); // Adjust this if the user can be in multiple rooms
-      });
+            delete users[socket.id]; // Remove user from the list
+            mainChat.to('General').emit('user list', Object.values(users)); // Update user list for all clients
+        });
     });
-    function updateUsersList(namespace, room) {
-      // Implement logic to update and broadcast the user list for the room
-      // This could involve tracking users in a data structure and emitting the updated list
-      namespace.to(room).emit('user list', /* Updated users list */);
-  }
 };
+
+
+
