99ecb6ec8a4e9a6c2438d62817fd0a660839b7b4
snoryder8019 <snoryder8019@gmail.com>
2024-07-24 16:17:39 +0000

    p2p socket files added

--- plugins/socket_io/setup.js
+++ plugins/socket_io/setup.js
@@ -3,6 +3,9 @@ const socketIo = require('socket.io');
 const configureNamespace = require('./namespace');
 const mainChatHandlers = require('./mainChat');
 const socketAdminHandlers = require('./socketAdmin');
+const socketP2PHandlers = require('./videoStream');
+
+
 const express = require('express')
 const router = express.Router();
 router.use((req,res,socketIo,next)=>{
@@ -17,6 +20,7 @@ const setupSocketIO = (server) => {
     const io = socketIo(server); 
     configureNamespace(io, '/main_chat', mainChatHandlers);
 configureNamespace(io,'/socketAdmin',socketAdminHandlers);
+configureNamespace(io,'p2p',socketP2PHandlers);
 };
 
 module.exports = {
---
+++ plugins/socket_io/videoStream.js
@@ -0,0 +1,15 @@
+//plugins/socket_io/videoStream.js
+/////////////////////
+//////////////////////
+//we want socket io to exchange the invite and asnswer and gather the ice candidates for p2p
+//socket emit 'p2pInit', emit 'p2pAnswer', 
+//design classes "client_window","client_camera", "peers", "peer", "P2Pcontrols", "P2Pcontrol"
+//client camera sits in client window, a peer sits in the peers, a button for P2Pcontrol sits in the P2Pcontrols
+const socketP2PHandlers = {
+    onConnection:(nsp, socket,users)=>{
+        const user =socket.request.user
+        const userName = user.firstName;
+        console.log(`VIDEOSTREAR.JS ~ User: ${userName} connected to videoStream`)
+    }
+}
+module.exports = socketP2PHandlers;
