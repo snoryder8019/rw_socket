bc66807100c4c0eb0523d1865553cc13aab2d940
Geoffrey Robinson <gmr5311@gmail.com>
2024-08-16 10:33:48 -0600

    get current tests into passing state

--- routes/adminFunctions/adminFunctions.js
+++ routes/adminFunctions/adminFunctions.js
@@ -11,9 +11,9 @@ const fs = require('fs')
 // hmm isAdmin Middleware
  function isAdmin(req, res, next) {
   let user = req.user;
-  console.log('ADMIN ACCESS: accessing admin routes: ' + user.displayName);
 
   if (user && user.isAdmin) {
+    console.log('ADMIN ACCESS: accessing admin routes: ' + user.displayName);
     next();
   } else {
     req.flash('error', 'Unauthorized. Please log in as an admin.');
@@ -23,7 +23,7 @@ const fs = require('fs')
 //use of flash and lib
   //  lib('card updated:', 'no errors from lib():', { cardID,userName }, 'cards.json','data');
  // req.flash('success', 'Card updated successfully.');
-   
+
 
 
 
