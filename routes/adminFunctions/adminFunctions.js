af5a1bf126fe29aca3d5b6ee39676650438b0341
Geoffrey Robinson <gmr5311@gmail.com>
2024-08-16 10:46:19 -0600

    add formatter to make exploring files easier

--- routes/adminFunctions/adminFunctions.js
+++ routes/adminFunctions/adminFunctions.js
@@ -1,16 +1,5 @@
-var express = require('express');
-var router = express.Router();
-const path = require('path')
-const upload = require('../../plugins/multer/setup');
-const { getDb } = require('../../plugins/mongo/mongo');
-const { ObjectId } = require('mongodb');
-const config = require('../../config/config'); // Import config if you're using it
-const lib = require('../logFunctions/logFunctions')
-const fs = require('fs')
-
-// hmm isAdmin Middleware
- function isAdmin(req, res, next) {
-  let user = req.user;
+function isAdmin(req, res, next) {
+  const user = req.user;
 
   if (user && user.isAdmin) {
     console.log('ADMIN ACCESS: accessing admin routes: ' + user.displayName);
@@ -20,14 +9,5 @@ const fs = require('fs')
     res.redirect('/');
   }
 }
-//use of flash and lib
-  //  lib('card updated:', 'no errors from lib():', { cardID,userName }, 'cards.json','data');
- // req.flash('success', 'Card updated successfully.');
-
-
-
-
-
-
 
 module.exports = { isAdmin };
